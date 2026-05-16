dcd4aa94595d3ce471cf7056c3a7df97c3e7a2f5
j <136126244+shopper12@users.noreply.github.com>
2026-05-16 14:28:21 +0900

    Remove unsafe Android plugin resolution override

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id.startsWith("com.android.")) {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
-        }
-    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
