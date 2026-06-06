0efbabc5bf6396e1119de4f7ee9f79b1b8cd62fa
j <136126244+shopper12@users.noreply.github.com>
2026-06-06 22:36:46 +0900

    Expose missed signal DAO

--- app/src/main/java/com/cryptotradecoach/data/local/AppDatabase.kt
+++ app/src/main/java/com/cryptotradecoach/data/local/AppDatabase.kt
@@ -28,6 +28,7 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun evolutionLogDao(): EvolutionLogDao
     abstract fun performanceCheckpointDao(): PerformanceCheckpointDao
     abstract fun watchOnlyDao(): WatchOnlyDao
+    abstract fun missedSignalDao(): MissedSignalDao
 
     companion object {
         @Volatile
